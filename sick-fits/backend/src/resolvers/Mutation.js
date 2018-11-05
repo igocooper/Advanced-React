ddd297893f9bd78f6a0dd9f7c3ced97a25b1955c
Oleg Lytvyn <igocooper@gmail.com>
2018-11-05 20:34:45 +0200

    playing around with Mutation and  Query

--- sick-fits/backend/src/resolvers/Mutation.js
+++ sick-fits/backend/src/resolvers/Mutation.js
@@ -1,3 +1,13 @@
-const Mutation = {};
+const Mutation = {
+  createDog(parent, {name}, ctx, info) {
+    const dog = {
+      name
+    };
+    global.dogs = global.dogs || [];
+    global.dogs = [...global.dogs, dog];
+
+    return dog
+  }
+};
 
 module.exports = Mutation;
